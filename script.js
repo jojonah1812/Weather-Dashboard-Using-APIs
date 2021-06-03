c9c6713427b423eec0f0682f5812c976c95a8b3f
Jank Jank <jank.jank1812@gmail.com>
2021-06-03 14:30:53 -0700

    adding changes to links

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 //--global variables
 var userApiKey = '5d050cc05f2e639349f41af0545c2086';
 
-var cityName = "Beaver Dam"
-var stateName = "WI"
+var cityName = ""
+var stateName = ""
 var stateCode = "US-"+ stateName
+var currentTemp
+var currentHumidity
+var cuurentWindSpeed
+var currentUvIndex
+var longitude
+var lattitude
+
+//--URL needs the concatonated variables to run the request.
+"https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&appid=" + userApiKey;
+
+
 var fetchButton = document.getElementById('fetch-button');
 
 function weatherFetch() {
   // fetch request gets data on requested city
-  //state code US-WI   https://en.wikipedia.org/wiki/ISO_3166-2:US
+  //state code = 'US-__"  https://en.wikipedia.org/wiki/ISO_3166-2:US
  
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={userApiKey}';
+    var requestUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&appid=" + userApiKey;
+    // var requestURL = https://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+ 
 
   fetch(requestUrl)
     .then(function (response) {
