3f8fc504f7862e7e7ac1755840f012c223344b1f
Jank Jank <jank.jank1812@gmail.com>
2021-06-02 18:23:52 -0700

    Added initial link to Weather API, created css, js files.

---
+++ script.js
@@ -0,0 +1,12 @@
+//--global variables
+var searchHistory
+var userApiKey = '5d050cc05f2e639349f41af0545c2086';
+
+
+function weatherFetch(){
+    // grab the name off the object we will be getting the info from
+}
+
+
+//--uneeded
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={5d050cc05f2e639349f41af0545c2086}
