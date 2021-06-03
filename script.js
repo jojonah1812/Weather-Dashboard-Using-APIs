2c90a246209130e538831a663a5d07a8e653cda3
Jank Jank <jank.jank1812@gmail.com>
2021-06-02 23:27:28 -0700

    added user API key, global variables, URL for request, added fetch and promises.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 //--global variables
-var searchHistory
 var userApiKey = '5d050cc05f2e639349f41af0545c2086';
 
+var cityName = "Beaver Dam"
+var stateName = "WI"
+var stateCode = "US-"+ stateName
+var fetchButton = document.getElementById('fetch-button');
 
-function weatherFetch(){
-    // grab the name off the object we will be getting the info from
+function weatherFetch() {
+  // fetch request gets data on requested city
+  //state code US-WI   https://en.wikipedia.org/wiki/ISO_3166-2:US
+ 
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={userApiKey}';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    })
 }
+//-- API call
+fetchButton.addEventListener('click', weatherFetch);
 
-
-//--uneeded
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={5d050cc05f2e639349f41af0545c2086}
+// 'https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={userApiKey}';
