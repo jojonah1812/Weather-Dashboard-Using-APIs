56500ac13610cfd557cb12faa8102cc99f49824d
Jank Jank <jank.jank1812@gmail.com>
2021-06-03 15:17:49 -0700

    figured out lat and log entry using the openweather geocoding API

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //--global variables
-var userApiKey = '5d050cc05f2e639349f41af0545c2086';
+var userApiKey="5d050cc05f2e639349f41af0545c2086";
+var cityName="Boston"
+var currentTemp=""
+var currentHumidity=""
+var currentWindSpeed=""
+var currentUvIndex=""
+var longitude=""
+var lattitude=""
 
-var cityName = ""
-var stateName = ""
-var stateCode = "US-"+ stateName
-var currentTemp
-var currentHumidity
-var cuurentWindSpeed
-var currentUvIndex
-var longitude
-var lattitude
+
+
+//--Geocoding lat & lon
+var requestGeo ="//api.openweathermap.org/geo/1.0/direct?q=" + cityName + ",US +&appid=" + userApiKey;
 
 //--URL needs the concatonated variables to run the request.
 "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&appid=" + userApiKey;
@@ -19,12 +21,7 @@ var fetchButton = document.getElementById('fetch-button');
 
 function weatherFetch() {
   // fetch request gets data on requested city
-  //state code = 'US-__"  https://en.wikipedia.org/wiki/ISO_3166-2:US
- 
-    var requestUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&appid=" + userApiKey;
-    // var requestURL = https://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
- 
-
+  var requestUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&appid=" + userApiKey;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -37,3 +34,8 @@ function weatherFetch() {
 fetchButton.addEventListener('click', weatherFetch);
 
 // 'https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={userApiKey}';
+// var requestURL = https://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+
+////Bryan said cut the state - it will run anyways ???
+// var stateName = "";
+// var stateCode =
